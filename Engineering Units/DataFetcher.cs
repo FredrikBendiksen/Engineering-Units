baec1f00c4b8a3cb80791350af1061b15589ca07
Trine Renee Berg <72825415+trinerb@users.noreply.github.com>
2022-11-21 13:04:41 +0100

    data interface

--- Engineering Units/DataFetcher.cs
+++ Engineering Units/DataFetcher.cs
@@ -3,17 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+
 
 namespace Engineering_Units
 {
     internal class DataFetcher
     {
+        readonly static string datasource = "XML";
         // Delegates dataFetcher-calls to the datasources
+        public static IFetchData CreateFetchData()
+        {
+            IFetchData data;
+            if (datasource == "XML")
+            {
+                data = new XMLReader();
+            }
+            else
+            {
+                data = new MockData();
+            }
+
+            return data;
+        }
+
+        public static List<string> GetQuantClass()
+        {
+            IFetchData data = CreateFetchData();
+            return data.GetQuantClass();
+
+        }
+
     }
 
-    internal class XMLReader
+    internal class XMLReader : IFetchData
     {
         // Reads XML-file
+        public List<string> GetQuantClass()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
---
+++ Engineering Units/MockData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engineering_Units
+{
+    internal class MockData : IFetchData
+    {
+        public List<string> GetQuantClass()
+        {
+            List<string> listOfTypes = new List<string>()
+            {
+               "quantity type1",
+               "quantity type2",
+               "quantity type3"
+             };
+
+
+            return listOfTypes;
+        }
+    }
+
+}
